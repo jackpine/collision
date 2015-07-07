ac829597bfa42a8283d21a99c3fe0cd4335263a6
Michael Kirk <michael.john.kirk@gmail.com>
2015-07-07 01:16:20 -0400

    limit incidents returned

--- api/app/controllers/api/v1/incidents_controller.rb
+++ api/app/controllers/api/v1/incidents_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IncidentsController < Api::BaseController
   def index
-    @incidents = Incident.all
+    @incidents = Incident.last(1000)
     respond_to do |format|
       format.json
     end
