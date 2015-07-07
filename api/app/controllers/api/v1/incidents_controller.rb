ffb1458a6a3abc79fe0d7131acb3d61e7fb820ad
Michael Kirk <michael.john.kirk@gmail.com>
2015-07-07 08:48:55 -0400

    return random sampling

--- api/app/controllers/api/v1/incidents_controller.rb
+++ api/app/controllers/api/v1/incidents_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IncidentsController < Api::BaseController
   def index
-    @incidents = Incident.last(50000)
+    @incidents = Incident.random_sampling(1000)
     respond_to do |format|
       format.json
     end
--- api/app/models/incident.rb
+++ api/app/models/incident.rb
@@ -1,4 +1,6 @@
 class Incident < ActiveRecord::Base
   validates_presence_of :switrs_case_id, :occurred_at, :killed_count, :injured_count
   validates_inclusion_of :bicycle_collision, :pedestrian_collision, in: [true, false], message: 'must be true or false'
+
+  scope :random_sampling, ->(limit) { order("RANDOM()").limit(limit) }
 end
