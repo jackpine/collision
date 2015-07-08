c272e88e98ae8eec5c930e5f12096b695f42e55d
Michael Kirk <michael.john.kirk@gmail.com>
2015-07-07 21:08:46 -0400

    working towards a functioning container

--- api/config/containers/api/run.sh
+++ api/config/containers/api/run.sh
@@ -3,6 +3,6 @@
 echo "running new container"
 docker run --name collision-api \
            --link collision-db:collision-db \
-           -p 4000:80 \
+           -p 80:80 \
            -d jackpine/collision-api
 
