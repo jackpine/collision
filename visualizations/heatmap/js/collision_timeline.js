dd7f3a4600df2ad2c766b420e9402fe0942e34f5
Michael Kirk <michael@jackpine.me>
2015-07-30 13:59:36 -0700

    split out heatmap, WIP on timeline component

--- visualizations/heatmap/js/collision_timeline.js
+++ visualizations/heatmap/js/collision_timeline.js
@@ -1,22 +1,34 @@
-function collisionTimeline() {
-    var context = document.getElementById('line-chart').getContext("2d");
+function CollisionTimeline(baseUrl, getBoundingBox) {
 
-    var data = {
-        labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009"],
-        datasets: [{
-            label: "Collisions",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        }]
-    };
+    var labels = ["2003", "2004", "2005", "2006", "2007", "2008", "2009"];
+    var points = [    65,     59,     80,     81,     56,     55,     40];
+    render(labels, points);
 
-    var options = {};
+    var labels = ["2003", "2004", "2005", "2006", "2007", "2008", "2009"];
+    var points2 = [    30,     60,     30,     60,     30,     60,     30];
+    render(labels, points2);
 
-    var myLineChart = new Chart(context).Line(data, options);
+    function render(labels, points) {
+
+        var context = document.getElementById('line-chart').getContext("2d");
+
+        var data = {
+            labels: labels,
+            datasets: [{
+                label: "Collisions",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: points
+            }]
+        };
+        var options = {};
+
+        // Is it ok to just keep overriding this? Do we need to blow away the existing one first?  //
+        var myLineChart = new Chart(context).Line(data, options);
+    }
 }
 
---
+++ visualizations/heatmap/js/heat_map.js
@@ -0,0 +1,53 @@
+function HeatMap(baseUrl, map, getBoundingBox) {
+  this.incidentsBaseUrl = baseUrl + 'incidents.json';
+  this.map = map;
+  this.getBoundingBox = getBoundingBox;
+}
+
+HeatMap.prototype.renderWithCurrentBounds = function() {
+  // The parameters we can tweak: blur, radius, maxzoom, intensity
+  //
+  // Because the vis looks quite different at differnt zooms and on different devices
+  // we have to check a lot of different combinations. Here's the zooms/devices I checked
+  // zoom-levels           (device)-> iPhone6, MacBook 13"
+  // 15 (neighborhood)
+  // 12 (city)
+  // 10 (metro)
+  // 7  (state)
+  var incidentsUrl = this.incidentsBaseUrl + "?bbox=" + this.getBoundingBox();
+  
+  var collisionMap = this.map;
+  // fetch incident data from the API
+  $.getJSON(incidentsUrl, function(json) {
+    //remove any existing heatlayer, otherwise we'll keep stacking layers every time we re-render
+    if (this.heatLayer) {
+      collisionMap.removeLayer(this.heatLayer);
+    }
+
+    var incidents = json.incidents;
+
+    // Some heatmap style parameters
+    var blur = 25;
+    var radius = blur;
+    var intensity = 0.10;
+
+    // Handle low incident presentaion specially, otherwise we can't see them.
+    if (incidents.length < 500) {
+      // The fewer the incidents, the more intensly we color any individual
+      // incident. This makes the visualizations intensity "relative" to how
+      // many incidents are currently on the map.
+      intensity += (0.4 * (1 - incidents.length/500))
+    }
+
+    console.log('incidents:', incidents.length, 'intensity:', intensity, 'blur:', blur, 'radius:', radius);
+
+    // Transform data from the API into a form the heatmap needs,
+    // Output looks like this: [ [34.2, 118.1, 0.3], [33.3, 118.9, 0.3], ...]
+    var incidentPoints = _.map(incidents, function(incidentAttributes) { return incidentAttributes.location.coordinates.reverse().concat([intensity]) });
+
+    // Build a layer from our points and put them on our map.
+    this.heatLayer = L.heatLayer(incidentPoints, {blur: blur, radius: radius, maxZoom: 13});
+    collisionMap.addLayer(this.heatLayer);
+  });
+};
+
