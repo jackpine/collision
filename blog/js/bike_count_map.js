61a67b8c962a721260d649d3408dbb2cadeaeea2
Michael Kirk <michael.john.kirk@gmail.com>
2015-11-06 17:39:50 -0800

    colored circles, radius is count

--- blog/js/bike_count_map.js
+++ blog/js/bike_count_map.js
@@ -1,4 +1,4 @@
-function BikeCountMap() {
+function BikeCountMap($el) {
   this.config = new Config();
   L.mapbox.accessToken = this.config.accessToken;
 
@@ -8,16 +8,49 @@ function BikeCountMap() {
   $.getJSON('/js/data/bike_counts_2015.geojson', function(data) {
     var calendar = _.groupBy(data.features, function(feature) { return feature.properties.started_at;})
 
+
+
+    var timeSelect = document.createElement('select');
+    $el.after(timeSelect);
+    
+
     var layers = {}
     _.each(calendar, function(features, startedAt) {
       var markers = _.map(features, function(feature) {
         var lonlat = feature.geometry.coordinates;
-        return L.marker(lonlat.reverse()).bindPopup("<p>" + feature.properties.total + "</p>");
+        var totalCount = feature.properties.total
+        var color = 'green';
+        if (totalCount == 0) {
+          color = 'white';
+        }
+        return L.circleMarker(lonlat.reverse(), { color: color, radius: totalCount + 2 } ).bindPopup("<p>" + totalCount + "</p>");
       });
       var layer = L.layerGroup(markers);
       layers[startedAt] = layer
-      layer.addTo(map);
+
+      var option = document.createElement('option');
+      option.value = option.text = startedAt;
+      timeSelect.add( option );
     });
+    var currentLayer;
+
+    timeSelect.addEventListener(
+      'change',
+      function() {
+        setTime(this.value);
+      },
+      false
+    )
+    setTime(Object.keys(calendar)[0]);
+
+    function setTime(time) {
+      if(currentLayer) {
+        map.removeLayer(currentLayer);
+      }
+      var layer = layers[time];
+      currentLayer = layer;
+      layer.addTo(map);
+    }
   });
 };
 
